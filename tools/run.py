302e801a38198c0cfa48dfe3faf2c9a5313c4dc8
coekjan <cn_yzr@qq.com>
2024-12-05 20:08:58 +0800

    Better way to specify kernel images

--- tools/run.py
+++ tools/run.py
@@ -22,8 +22,8 @@ def args(parser: argparse.ArgumentParser):
                         help='The amount of RAM to allocate in GB')
     parser.add_argument('--port', type=int, default=8022,
                         help='The port to forward SSH to')
-    parser.add_argument('--kernel', type=validate_filename_arg, default='Image',
-                        help='The path to the kernel image')
+    parser.add_argument('--kernel', type=validate_filename_arg,
+                        help='The name of the kernel image')
 
 
 def do(arch: str, **kwargs):
@@ -41,7 +41,7 @@ def do_aarch64(name: str,
                smp: int,
                ram: int,
                port: int,
-               kernel: str):
+               kernel: str | None):
     from .utils.qemu import run_aarch64_linux as run
 
     goto_workspace('aarch64', name)
@@ -50,8 +50,12 @@ def do_aarch64(name: str,
         raise FileNotFoundError(
             'Disk image not found, please run `init` first')
 
-    if not pathlib.Path(kernel).exists():
-        kernel = None
+    if kernel is not None:
+        if not pathlib.Path(kernel).exists():
+            if pathlib.Path(f'Image-{kernel}').exists():
+                kernel = f'Image-{kernel}'
+            else:
+                raise FileNotFoundError(f'Kernel image `{kernel}` not found')
 
     run(
         qemu,
